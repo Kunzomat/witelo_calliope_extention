b5a679025bc023b0609cd506347e5c001ff596de
Andreas Brisgen <akunze_de@web.de>
2026-03-12 15:27:32 +0100

    Refactor sim.ts by removing unused functions
    Removed unused functions and modified drawText logging.

--- sim.ts
+++ sim.ts
@@ -1,18 +1,7 @@
 namespace pxsim.witelo {
 
-    console.log("SIMULATOR LÄUFT")
-
-    export function clear() {
-        console.log("clear() aufgerufen")
-    }
-
     export function drawText(text: string, x: number, y: number) {
-        console.log("drawText:", text, x, y)
-    }
-
-    export function temperature_bmp280(): number {
-        console.log("temperature_bmp280()")
-        return 42
+        console.log("SIM drawText:", text)
     }
 
 }
